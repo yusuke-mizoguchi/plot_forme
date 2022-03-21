94e62dd37a43cbb78b0bde48b6a67bfbf6b27963
yusuke-mizoguchi <zunmokou@gmail.com>
2022-03-21 22:17:53 +0900

    Add:novel_updateにコードを追加

--- app/models/novel_update_form.rb
+++ app/models/novel_update_form.rb
@@ -15,11 +15,55 @@ class NovelCreateForm
   #attributeがtextに対応していない為切り分け。実装されたら統一
   attr_accessor :plot, :genre, :story_length, :release, :character_text
 
+  delegate :persisted?, to: :@novel
+
   #characterのバリデーション
   validates :character_text, length: { maximum: 2000 }
   validates :character_role, length: { maximum:20 }
 
-  def update
 
+  def to_model
+    @novel
   end
+
+  def initialize(attributes = {}, novel: Novel.new)
+    @novel = novel
+    new_attributes = default_attributes.merge(attributes)
+    super(new_attributes)
+  end
+
+  def update
+    return false if invalid?
+
+    ActiveRecord::Base.transaction do
+      @novel.update!(novel_params)
+
+      @novel.character_role = character_role
+      @novel.character_text = character_text
+    end
+  end
+
+  private
+
+  def novel_params
+    {
+      title: title, genre: genre, story_length: story_length,
+      plot: plot, image: image, release: release, user_id: user_id
+    }
+  end
+
+  def default_attributes
+    {
+      title: @novel.title,
+      genre: @novel.genre,
+      story_length: @novel.story_length,
+      plot: @novel.plot,
+      image: @novel.image,
+      release: @novel.release,
+      user_id: @novel.user_id,
+      character_role: @novel.character,
+      character_text: @novel.character
+    }
+  end
+
 end
