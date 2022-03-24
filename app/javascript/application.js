41589e1521db0f4fcd535448e500faae48a38fa3
yusuke-mizoguchi <zunmokou@gmail.com>
2022-03-24 23:31:53 +0900

    Add:cocoonでフォームを作成(未完)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,10 @@
 // Entry point for the build script in your package.json
+require("jquery")
+require("@nathanvda/cocoon")
+require ("src/cocoon")
+
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
-
-require("jquery")
-require("@nathanvda/cocoon")
+import 'cocoon-js';
+import "@nathanvda/cocoon"
